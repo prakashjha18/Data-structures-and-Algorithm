56e17abbfd4818602712b01565dc2e7bcb8d46f6
prakash jha <prkjha25@gmail.com>
2020-01-31 15:21:14 +0530

    staircase problem with fee associated for every stair

---
+++ Dynamic Programming/stairwaytoheaven2.java
@@ -0,0 +1,21 @@
+public class stairwaytoheaven2 
+{
+	public static void main(String args[]) {
+
+	    stairwaytoheaven2 sth2 = new stairwaytoheaven2();
+            int[] fee = {2, 1, 3, 1, 2};
+	        System.out.println(sth2.calculate(fee, 5));
+	}
+	private int calculate(int[] fee, int n){
+	    int[] minFee = new int[n + 1];
+	    // Base cases
+	    minFee[0] = 0;
+	    minFee[1] = minFee[2] = fee[0];
+	    // 'i' sets the stair number
+	    for(int i = 3; i <= n; i++){
+	        // general recurrence relation
+	        minFee[i] = Math.min(minFee[i - 1] + fee[i - 1], Math.min(minFee[i - 2] + fee[i - 2], minFee[i - 3] + fee[i - 3]));
+	    }
+	    return minFee[n];
+	}
+}
